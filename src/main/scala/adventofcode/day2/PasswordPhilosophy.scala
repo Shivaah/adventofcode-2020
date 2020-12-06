e6eb54f4397adbbe44f2f9e7aa5c9a352a50e6b7
Shivaah <seikay83@gmail.com>
2020-12-06 16:39:37 +0100

    Add day2 part2 solution and do small refactoring

--- src/main/scala/adventofcode/day2/PasswordPhilosophy.scala
+++ src/main/scala/adventofcode/day2/PasswordPhilosophy.scala
@@ -4,17 +4,26 @@ import scala.io.Source
 
 case class PasswordPolicy(min : Int, max : Int, letter : Char) {
 
-  def validate(password : String) : Boolean = {
+  def validateWithCount(password : String) : Boolean = {
     val result = password.filter(x => x == letter)
 
     result.length >= min && result.length <= max
   }
 
+  def validateWithPositions(password : String) : Boolean = {
+    val candidates = password.charAt(min - 1) :: password.charAt(max - 1) :: Nil
+
+    areRightLetters(candidates) || areRightLetters(candidates.reverse)
+  }
+
+  private def areRightLetters(candidates : Seq[Char]) : Boolean =
+    candidates.head == letter && candidates.last != letter
+
 }
 
 object PasswordPolicy {
 
-  def createFromRaw(rawPolicy : String) : PasswordPolicy = {
+  def from(rawPolicy : String) : PasswordPolicy = {
     val result = rawPolicy.split(" ")
     val (min, max) = result(0).split('-') match { case Array(f1, f2) => (f1.toInt, f2.toInt) }
     val letter = result(1).charAt(0)
@@ -23,7 +32,6 @@ object PasswordPolicy {
   }
 
 }
-
 
 object PasswordPhilosophy {
 
@@ -36,14 +44,30 @@ object PasswordPhilosophy {
     formattedPasswordPolicy :: trimPassword :: Nil
   }
 
+  def getPasswordAndPolicyFrom(entry : String) : (String, PasswordPolicy) = {
+    val parsedEntry = parse(entry)
+    val password = parsedEntry.last
+    val policy = PasswordPolicy.from(parsedEntry.head)
+
+    (password, policy)
+  }
+
   def getPart1(input : Seq[String]) : Int = {
     val result = input.filter {
       x => {
-        val parsedEntry = parse(x)
-        val password = parsedEntry.last
-        val policy = PasswordPolicy.createFromRaw(parsedEntry.head)
+        val (password, policy) = getPasswordAndPolicyFrom(x)
+        policy.validateWithCount(password)
+      }
+    }
 
-        policy.validate(password)
+    result.length
+  }
+
+  def getPart2(input : Seq[String]) : Int = {
+    val result = input.filter {
+      x => {
+        val (password, policy) = getPasswordAndPolicyFrom(x)
+        policy.validateWithPositions(password)
       }
     }
 
@@ -54,8 +78,9 @@ object PasswordPhilosophy {
     val bufferedResource = Source.fromResource("day2.txt")
     val input = bufferedResource.getLines().toSeq
     bufferedResource.close()
-    
+
     println(getPart1(input))
+    println(getPart2(input))
   }
 
 }
