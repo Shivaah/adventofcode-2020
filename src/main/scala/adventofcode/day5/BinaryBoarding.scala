41fa1161dd1df6f923aad4f38cfc2590fc65986c
Shivaah <seikay83@gmail.com>
2020-12-11 23:54:02 +0100

    Add day5 part1 solution

---
+++ src/main/scala/adventofcode/day5/BinaryBoarding.scala
@@ -0,0 +1,50 @@
+package adventofcode.day5
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object BinaryBoarding {
+  type SearchPattern = (Char, Char)
+
+  def halfRegion(pattern: SearchPattern)(range : Range, letter: Char): Range = {
+    val (forUpperHalf, forLowerHalf) = pattern
+
+    letter match {
+      case `forUpperHalf` => range.start + (range.length / 2) to range.end
+      case `forLowerHalf` => range.start to range.end - (range.length / 2)
+      case _ => range
+    }
+  }
+
+  @tailrec
+  def find(pattern: SearchPattern)(range : Range, displacement: List[Char]): Option[Int] = {
+    val (forUpperHalf, forLowerHalf) = pattern
+
+    displacement match {
+      case Nil => None
+      case head :: Nil => head match {
+        case `forUpperHalf` => Some(range.end)
+        case `forLowerHalf` => Some(range.start)
+        case _ => None
+      }
+
+      case head :: tail => find(pattern)(halfRegion(pattern)(range, head), tail)
+    }
+  }
+
+  def row(range : Range, displacement: List[Char]): Option[Int] = find('B', 'F')(range, displacement)
+  def column(range : Range, displacement: List[Char]): Option[Int] = find('R', 'L')(range, displacement)
+
+  def getPart1(input : Seq[String]): Int =
+    input.map(x => (x.slice(0, 7).toList, x.slice(7, input.head.length).toList))
+      .map(x => row(0 to 127, x._1).getOrElse(0) * 8 + column(0 to 7, x._2).getOrElse(0))
+      .max
+
+  def main(args: Array[String]): Unit = {
+    val bufferedResource = Source.fromResource("day5.txt")
+    val input = bufferedResource.getLines().toList
+    bufferedResource.close()
+
+    println(getPart1(input))
+  }
+}
